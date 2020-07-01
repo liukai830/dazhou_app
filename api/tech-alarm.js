74b65229610542e25438a9668f45c13c9880d6c6
liuk <liukai830@163.com>
2020-07-01 18:14:35 +0800

    工艺报警api

--- api/tech-alarm.js
+++ api/tech-alarm.js
@@ -1,15 +1,23 @@
 import http from '../common/interface'
 
+/* 查询工艺报警信息（单个角色） */
 export const getSingleRoleTechAlarmList = (param) => {
     return http.post('Api/Machine/GetTechnologicalProcessList', param)
 }
 
+/* 查询工艺报警信息（多个角色） */
 export const getMultiRoleTechAlarmList = (paramArray) => {
 		const promises = paramArray.map(param => http.post('Api/Machine/GetTechnologicalProcessList',param))
 		return Promise.all(promises)
 }
 
+/* 处理工艺报警信息 */
+export const handleTechAlarm = (params) => {
+	return http.post('Api/Machine/HandleProcess', param)
+}
+
 export default {
 	getSingleRoleTechAlarmList,
-	getMultiRoleTechAlarmList
+	getMultiRoleTechAlarmList,
+	handleTechAlarm
 }
